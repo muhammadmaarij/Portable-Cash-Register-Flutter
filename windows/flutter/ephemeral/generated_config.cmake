651f87170393a3dbee9b4411e290542c5350a52d
Muhammad Maarij <84044169+muhammadmaarij@users.noreply.github.com>
2022-06-02 21:40:38 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Muhammad Maarij\\AndroidStudioProjects\\Portable Cash Register\\portable_cash_register" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\Muhammad Maarij\\AndroidStudioProjects\\Portable Cash Register\\portable_cash_register"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Muhammad Maarij\\AndroidStudioProjects\\Portable Cash Register\\portable_cash_register\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Muhammad Maarij\\AndroidStudioProjects\\Portable Cash Register\\portable_cash_register"
+  "FLUTTER_TARGET=C:\\Users\\Muhammad Maarij\\AndroidStudioProjects\\Portable Cash Register\\portable_cash_register\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Muhammad Maarij\\AndroidStudioProjects\\Portable Cash Register\\portable_cash_register\\.dart_tool\\package_config.json"
+)
